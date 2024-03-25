5a3868c78f2ce19d0c428cf092a8d36f7444964b
kirillshavlovskiy <kirillshavlovsky@me.com>
2024-03-26 01:23:48 +0300

    Added some changes to the base app, installed mui/material lib

--- demo/src/layout/index.js
+++ demo/src/layout/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import Header from './Header';
 import Content from './Content';
-
+import Structure from './Structure';
 import { ThemeProvider } from 'theme';
 
 const Layout = _ => <ThemeProvider>
   <section className="full-size">
     <Header />
     <Content />
-
+    <Structure />
   </section>
 </ThemeProvider>;
 
--- demo/src/sections/Structure/CustomizedTreeView.js
+++ demo/src/sections/Structure/CustomizedTreeView.js
@@ -1,61 +1,60 @@
-import React from 'react';
-import { useSpring, animated } from '@react-spring/web';
-import { Box } from '@material-ui/core';
-import SvgIcon from '@material-ui/core/SvgIcon';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import RemoveIcon from '@material-ui/icons/Remove';
-
-import Collapse from '@material-ui/core/Collapse';
-import { fade, withStyles, makeStyles } from '@material-ui/core/styles';
-import { TreeView, TreeItem } from '@material-ui/lab';
-
-// ... <MinusSquare />, <PlusSquare /> and <CloseSquare /> components code here ...
-
-function TransitionComponent(props) {
-  const style = useSpring({
-    to: { opacity: props.in ? 1 : 0, transform: `translate3d(${props.in ? 0 : 20}px,0,0)` },
-  });
-
-  return (
-    <animated.div style={style}>
-      <Collapse {...props} />
-    </animated.div>
-  );
-}
+import React, { useState } from 'react';
+import { Box, Drawer, IconButton, withStyles } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import RemoveIcon from '@mui/icons-material/Remove';
+import MenuIcon from '@mui/icons-material/Menu';
+import { TreeView, TreeItem } from '@mui/lab';
+import { alpha } from '@mui/system';
 
 const styles = theme => ({
-  root: {
-    '&.MuiTreeItem-root > .MuiTreeItem-content > .MuiTreeItem-iconContainer .close': {
-      opacity: 0.3,
-    },
-    '&.MuiTreeItem-root .MuiTreeItem-group': {
-      marginLeft: 15,
-      paddingLeft: 18,
-      borderLeft: `1px dashed ${fade(theme.palette.text.primary, 0.4)}`,
-    },
-  }
+    root: {
+        '&.MuiTreeItem-root > .MuiTreeItem-content > .MuiTreeItem-iconContainer .close': {
+            opacity: 0.3,
+        },
+        '&.MuiTreeItem-root .MuiTreeItem-group': {
+            marginLeft: 15,
+            paddingLeft: 18,
+            borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
+        },
+    }
 });
 
 const StyledTreeItem = withStyles(styles)(({ classes, ...others }) => (
-  <TreeItem {...others} TransitionComponent={TransitionComponent} className={classes.root} />
+    <TreeItem {...others} className={classes.root} />
 ));
 
 export default function CustomizedTreeView() {
-  return (
-    <Box sx={{ minHeight: 270, flexGrow: 1, maxWidth: 300 }}>
-    <TreeView
-      aria-label="customized"
-      defaultExpanded={['1']}
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      defaultEndIcon={<RemoveIcon />}
-      style={{ overflowX: 'hidden', minHeight: 270, flexGrow: 1, maxWidth: 300 }}
-    >
-      <StyledTreeItem nodeId="1" label="Main">
-        {/* Other Tree Items */}
-      </StyledTreeItem>
-    </TreeView>
-    </Box>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setIsOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setIsOpen(false);
+    };
+
+    return (
+        <Box sx={{ minHeight: 270, flexGrow: 1, maxWidth: 300 }}>
+            <IconButton edge="start" color="inherit" onClick={handleDrawerOpen}>
+                <MenuIcon />
+            </IconButton>
+
+            <Drawer anchor="left" open={isOpen} onClose={handleDrawerClose}>
+                <TreeView
+                    aria-label="customized"
+                    defaultExpanded={['1']}
+                    defaultCollapseIcon={<ExpandMoreIcon />}
+                    defaultExpandIcon={<ChevronRightIcon />}
+                    defaultEndIcon={<RemoveIcon />}
+                    style={{ overflowX: 'hidden', minHeight: 270, flexGrow: 1, maxWidth: 300 }}
+                >
+                    <StyledTreeItem nodeId="1" label="Main">
+                        {/* Other Tree Items */}
+                    </StyledTreeItem>
+                </TreeView>
+            </Drawer>
+        </Box>
+    );
 }
